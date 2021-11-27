943e77f56c83e9f840094c24823f526b97a3e287
tohsaka888 <156132264@qq.com>
2021-11-27 16:16:05 +0800

    增加主页

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,7 @@
-import React, { useEffect, useState } from "react";
-import ReactECharts from "echarts-for-react";
-import { lineChartsOption } from "./charts/lineChartsOption";
-import { pieChartsOption } from "./charts/pieChartsOption";
-import { getTagDetail } from "./request/getTagDetail";
-import { Empty, Tabs } from "antd-mobile";
 import "./App.css";
 
-type ResponseProp = {
-  name: string;
-  value: number;
-  textStyle?: any;
-};
-
 function App() {
-  const [show, setShow] = useState<boolean>(false);
-  useEffect(() => {
-    const getDetail = async () => {
-      const data: ResponseProp[] = await getTagDetail();
-      pieChartsOption.series[0].data = data.slice();
-      pieChartsOption.legend.data = data.slice();
-      console.log(pieChartsOption.series[0].data);
-      let xValue: string[] = [];
-      data.map((item, index) => {
-        xValue.push(item.name);
-        return null;
-      });
-      lineChartsOption.xAxis.data = xValue;
-      // data.textStyle = { color: "white" };
-      // lineChartsOption.yAxis.data = data;
-      lineChartsOption.series[0].data = data;
-      setShow(true);
-    };
-    getDetail();
-  }, []);
-
-  return (
-    <div className="charts-container">
-      <Tabs activeLineMode="full">
-        <Tabs.Tab title="折线图" key={0}>
-          {show ? (
-            <ReactECharts option={lineChartsOption} style={{ margin: "2vw" }} />
-          ) : (
-            <Empty
-              style={{ padding: "64px 0" }}
-              imageStyle={{ width: 128 }}
-              description="暂无数据"
-            />
-          )}
-        </Tabs.Tab>
-
-        <Tabs.Tab title="饼状图" key={1}>
-          {show ? (
-            <ReactECharts
-              option={pieChartsOption}
-              style={{ margin: "2vw", height: "400px" }}
-            />
-          ) : (
-            <Empty
-              style={{ padding: "64px 0" }}
-              imageStyle={{ width: 128 }}
-              description="暂无数据"
-            />
-          )}
-        </Tabs.Tab>
-      </Tabs>
-    </div>
-  );
+  return <div>Welcome to Ehcharts Webview!</div>;
 }
 
 export default App;
