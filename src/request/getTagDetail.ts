71047a45bac67faf10eafc1fee2253b56bdfef3a
tohsaka888 <156132264@qq.com>
2021-11-27 16:15:51 +0800

    修改获取表格数据请求

--- src/request/getTagDetail.ts
+++ src/request/getTagDetail.ts
@@ -1,13 +1,14 @@
 import { pythonUrl } from "./baseUrl";
 
-const getTagDetail = async () => {
-  const res = await fetch(`${pythonUrl}/getDCCI`, {
+const getTagDetail = async (url: string = '') => {
+  const res = await fetch(`${pythonUrl}/getDataAnalyse`, {
     method: "POST",
     headers: { Accept: "application/json", "Content-Type": "application/json" },
     mode: "cors",
+    body: JSON.stringify({ url: url }),
   });
   const data = await res.json();
-  return data.result;
+  return data.data[0].result || [];
 };
 
 export { getTagDetail };
